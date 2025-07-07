ae0be9cd4c9c49590fa0d4b0bc36bfb246c7af9c
Saroj12Dangol <saroj9823dangol@gmail.com>
2025-07-07 06:59:02 +0545

    no bg in blog detail

--- app/blogs/[slug]/layout.tsx
+++ app/blogs/[slug]/layout.tsx
@@ -1,10 +1,9 @@
-import BackgroundCanvas from "@/components/background-canvas";
 import React from "react";
 
 const BlogDetailLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <BackgroundCanvas />
+      {/* <BackgroundCanvas /> */}
       <main className="bg-gray-950 min-h-screen">{children}</main>
     </>
   );
