6d7ba811f86684131dd88a3680f9e2e2958c9ebd
Saroj12Dangol <saroj9823dangol@gmail.com>
2025-07-07 07:37:58 +0545

    feat: google tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 import { siteConfig } from "@/config";
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
+import Script from "next/script";
 import type { ReactNode } from "react";
 import "./globals.css";
 
@@ -100,13 +101,28 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         />
         <link rel="dns-prefetch" href="https://fonts.googleapis.com" />
         <link rel="dns-prefetch" href="https://fonts.gstatic.com" />
-        {/* <link
-          rel="alternate"
-          href="https://sarojdangol012.com.np"
-          hrefLang="x-default"
-        /> */}
+
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-53ZHCQL2')`,
+          }}
+        />
       </head>
       <body className="min-h-screen bg-background antialiased">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-53ZHCQL2"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <ThemeProvider
           attribute={siteConfig.theme.attribute}
           defaultTheme={siteConfig.theme.defaultTheme}
