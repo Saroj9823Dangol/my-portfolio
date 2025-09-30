fafa65667c81be8f539a47380704ba09cfe9e13f
Saroj12Dangol <saroj9823dangol@gmail.com>
2025-09-30 16:30:30 +0545

    fix: next-config issue

--- next.config.js
+++ next.config.js
@@ -1,41 +1,41 @@
 // /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   output: "export",
-//   eslint: {
-//     ignoreDuringBuilds: true,
-//   },
-//   typescript: {
-//     ignoreBuildErrors: true,
-//   },
-//   images: {
-//     unoptimized: true,
-//   },
-// };
+const nextConfig = {
+  output: "export",
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+};
 
 // export default nextConfig;
 
-const withPWA = require("next-pwa")({
-  disable: process.env.NODE_ENV === "development", // Disable in dev
-  swDest: "export",
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  runtimeCaching: [
-    {
-      urlPattern: /^https?.*/,
-      handler: "NetworkFirst",
-      options: {
-        cacheName: "offlineCache",
-        expiration: {
-          maxEntries: 200,
-        },
-      },
-    },
-  ],
-});
+// const withPWA = require("next-pwa")({
+//   disable: process.env.NODE_ENV === "development", // Disable in dev
+//   swDest: "export",
+//   dest: "public",
+//   register: true,
+//   skipWaiting: true,
+//   runtimeCaching: [
+//     {
+//       urlPattern: /^https?.*/,
+//       handler: "NetworkFirst",
+//       options: {
+//         cacheName: "offlineCache",
+//         expiration: {
+//           maxEntries: 200,
+//         },
+//       },
+//     },
+//   ],
+// });
 
-module.exports = withPWA({
-  // Your existing Next.js config
-  reactStrictMode: true,
-  swcMinify: true,
-});
+// module.exports = withPWA({
+//   // Your existing Next.js config
+//   reactStrictMode: true,
+//   swcMinify: true,
+// });
